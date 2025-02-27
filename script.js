ea9073a11a91fc2562cad015ae56e29efd7db06d
ha-rshh <prajapati.captain@gmail.com>
2025-02-27 19:22:44 +0530

    testing new js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-Polyfill for indexOF
-//
 
 const userName = document.getElementById("name");
 const subject = document.getElementById("title");
@@ -7,28 +5,18 @@ const review = document.getElementById("review");
 const submitBtn = document.getElementById("submit");
 const reviews = document.getElementById("reviews-list");
 const newReview = document.createElement("li");
-newReview.textContent = ` <span class="user-name"> ${userName.value} </span>
-              <span class="user-rating">
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-regular fa-star"></i>
-                <i class="fa-regular fa-star"></i>
-              </span>
 
-              <div class="user-title">
-               ${subject.value}
-              </div>
-              <div class="user-review">
-                ${review}
-              </div>`;
 
-submitBtn.addEventListener("click", (e) => {
-  if (!userName.value || !subject.value || !review.value) {
-    console.log("please enter somthing");
-  } else {
-    console.log(userName.value, subject.value, review.value);
-  }
-  
-});
+const userReview = [{
+    userKaNaam : userName.value,
+    reveiwKaSubject : subject.value,
+    userKaReview : review.value,
+}]
 
+submitBtn.addEventListener("click", e =>{
+
+    // console.log(userReview.userKaNaam)
+    console.log(userName.value);
+    
+    
+})
