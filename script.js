43762857132cdae28f14e9963fd58abbfe003d2b
ha-rshh <prajapati.captain@gmail.com>
2025-03-17 14:11:34 +0530

    deleted index2.html

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
+const userName = document.getElementById("name")
+const title = document.getElementById("title")
+const review = document.getElementById("review")
+const submitBtn = document.getElementById("submit")
 
-const userName = document.getElementById("name");
-const subject = document.getElementById("title");
-const review = document.getElementById("review");
-const submitBtn = document.getElementById("submit");
-const reviews = document.getElementById("reviews-list");
-const newReview = document.createElement("li");
+const reviewListContainer = document.getElementById('review-list-container')
 
-
-const userReview = [{
-    userKaNaam : userName.value,
-    reveiwKaSubject : subject.value,
-    userKaReview : review.value,
+const reviewArray = [{
+  username : userName.value,
+  reviewTitle : title.value,
+  reviews: review.value
 }]
 
-submitBtn.addEventListener("click", e =>{
 
-    // console.log(userReview.userKaNaam)
-    console.log(userName.value);
-    
-    
-})
+submitBtn.addEventListener("click", ()=>{
+ if(userName.value == ""){
+    alert("please enter the username")
+ } else if(title.value == ""){
+    alert("please enter the headline")
+ } else if(review.value == ""){
+    alert("please enter the review")
+ } else {
+  const newItem = document.createElement("div")
+  newItem.innerHTML = reviewArray.userName
+  reviewListContainer.innerHTML = newItem
+ }
+})  
+
